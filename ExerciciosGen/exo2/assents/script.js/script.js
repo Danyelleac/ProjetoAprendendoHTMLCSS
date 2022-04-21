01a7846a2f78db418ad3ebdd213653cf2a7e63f2
DanyelleAC <danyelleacandido@gmail.com>
2022-04-21 12:01:36 -0300

    atualizando js

--- ExerciciosGen/exo2/assents/script.js/script.js
+++ ExerciciosGen/exo2/assents/script.js/script.js
@@ -1,26 +1,26 @@
 function validar() {
-    var email = document.querySelector("#email");
-    var subject = document.querySelector("#subject");
-    var emailRegex = validarEmail(email.value);
-  
-    if (emailRegex) {
-      alert("Erro no envio: Endereço de E-mail inválido");
-      email.focus();
-      
-    } else if (subject.value === "") {
-      alert("Erro no envio: Insira uma mensagem");
-      subject.focus();
-    } else {
-      
-        alert("Obrigada pelo contato " + email.value.substring(0, 32));
-    }
+  var email = document.querySelector("#email");
+  var subject = document.querySelector("#subject");
+  var emailRegex = validarEmail(email.value);
+
+  if (emailRegex) {
+    alert("Erro no envio: Endereço de E-mail inválido");
+    email.focus();
+    
+  } else if (subject.value === "") {
+    alert("Erro no envio: Insira uma mensagem");
+    subject.focus();
+  } else {
+    
+      alert("Obrigada pelo contato " + email.value.substring(0, 32));
   }
-  
-  function validarEmail(email) {
-    var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-    if (reg.test(email)) {
-      return false;
-    } else {
-      return true;
-    }
+}
+
+function validarEmail(email) {
+  var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+  if (reg.test(email)) {
+    return false;
+  } else {
+    return true;
   }
+}
