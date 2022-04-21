3f7ca5640d8bb5977ab079450b86994b8f43c453
DanyelleAC <danyelleacandido@gmail.com>
2022-04-21 12:10:43 -0300

    atualizando js

--- ExerciciosGen/exo2/assents/script.js/script.js
+++ ExerciciosGen/exo2/assents/script.js/script.js
@@ -1,23 +1,25 @@
 function validar() {
   var email = document.querySelector("#email");
   var subject = document.querySelector("#subject");
+  var name = document.querySelector("#name");
   var emailRegex = validarEmail(email.value);
 
   if (emailRegex) {
-    alert("Erro no envio: Endereço de E-mail inválido");
-    email.focus();
-    
-  } else if (subject.value === "") {
-    alert("Erro no envio: Insira uma mensagem");
-    subject.focus();
+    alert("E-mail inválido");
+    email.focus(); 
+    } else if (subject.value == "") {
+      alert("Insira uma mensagem");
+      subject.focus();
+      } else if (name.value === "") {
+        alert("Insira uma mensagem");
+        name.focus();
   } else {
-    
-      alert("Obrigada pelo contato " + email.value.substring(0, 32));
+    alert("Obrigada pelo contato " + name.value);
   }
 }
 
 function validarEmail(email) {
-  var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+  var reg = /(\w+([-+.']\w+)*){0,17}@{1}(\w+([-.]\w+)*\.\w+([-.]\w+)*){0,15}/;
   if (reg.test(email)) {
     return false;
   } else {
